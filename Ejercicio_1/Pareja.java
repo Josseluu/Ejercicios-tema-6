6c1570ce02f9aa11bc93ac39f08410c70564f8a3
Josseluu <jslslpz@myuax.com>
2024-05-07 20:02:09 +0200

    Initial commit

--- Ejercicio_1/Pareja.java
+++ Ejercicio_1/Pareja.java
@@ -1,21 +1,27 @@
-public class Pareja<T> {
+package Ejercicio_1;
+
+class Pareja<T> {
     private T primerDato;
     private T segundoDato;
 
+    // Constructor
     public Pareja(T primerDato, T segundoDato) {
         this.primerDato = primerDato;
         this.segundoDato = segundoDato;
     }
 
+    // Método para obtener el primer dato
     public T obtenerPrimerDato() {
         return primerDato;
     }
 
+    // Método para obtener el segundo dato
     public T obtenerSegundoDato() {
         return segundoDato;
     }
 
     public static void main(String[] args) {
+        // Ejemplo de uso
         Pareja<Integer> parejaEnteros = new Pareja<>(10, 20);
         System.out.println("Primer dato: " + parejaEnteros.obtenerPrimerDato());
         System.out.println("Segundo dato: " + parejaEnteros.obtenerSegundoDato());
